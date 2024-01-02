36e10bf9f20ddc1b063161a56035b73a3ab3bbae
ngocanhkk1809 <130637222+ngocanhkk1809@users.noreply.github.com>
2024-01-02 22:47:56 +0700

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -15,13 +15,11 @@ from engine import *
 from models import build_model
 import os
 import warnings
-
 warnings.filterwarnings('ignore')
-
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Set parameters for P2PNet evaluation', add_help=False)
-
+    
     # * Backbone
     parser.add_argument('--backbone', default='vgg16_bn', type=str,
                         help="name of the convolutional backbone to use")
@@ -31,20 +29,21 @@ def get_args_parser():
     parser.add_argument('--line', default=2, type=int,
                         help="line number of anchor points")
 
-    parser.add_argument('--output_dir', default='./logs',
+    parser.add_argument('--output_dir', default='',
                         help='path where to save')
-    parser.add_argument('--weight_path', default='./ckpt/best_mae.pth',  # './weights/SHTechA.pth',  #
+    parser.add_argument('--weight_path', default='',
                         help='path where the trained weights saved')
 
     parser.add_argument('--gpu_id', default=0, type=int, help='the gpu used for evaluation')
 
     return parser
 
-
 def main(args, debug=False):
+
     os.environ["CUDA_VISIBLE_DEVICES"] = '{}'.format(args.gpu_id)
+
     print(args)
-    device = torch.device('cuda')  # cuda
+    device = torch.device('cuda')
     # get the P2PNet
     model = build_model(args)
     # move to GPU
@@ -57,18 +56,14 @@ def main(args, debug=False):
     model.eval()
     # create the pre-processing transform
     transform = standard_transforms.Compose([
-        standard_transforms.ToTensor(),
+        standard_transforms.ToTensor(), 
         standard_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     # set your image path here
-    img_path = "./vis/demo4.jpg"
+    img_path = "./vis/demo1.jpg"
     # load the images
     img_raw = Image.open(img_path).convert('RGB')
-
-    (width, height) = (round(img_raw.width / 1.5), round(img_raw.height / 1.5))
-    img_raw = img_raw.resize((width, height))
-
     # round the size
     width, height = img_raw.size
     new_width = width // 128 * 128
@@ -80,8 +75,7 @@ def main(args, debug=False):
     samples = torch.Tensor(img).unsqueeze(0)
     samples = samples.to(device)
     # run inference
-    with torch.no_grad():
-        outputs = model(samples)
+    outputs = model(samples)
     outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
 
     outputs_points = outputs['pred_points'][0]
@@ -101,7 +95,6 @@ def main(args, debug=False):
         img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
     # save the visualized image
     cv2.imwrite(os.path.join(args.output_dir, 'pred{}.jpg'.format(predict_cnt)), img_to_draw)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('P2PNet evaluation script', parents=[get_args_parser()])
